69c6e3721b549e44767bea8feb2defbcca52d3ce
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-10-27 21:04:50 -0300

    Update depedencies

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo && echo "Install dependencies"
-sudo apt install -y build-essential clang cmake git libssl-dev libgtk-3-dev libsoup2.4-dev libwebkit2gtk-4.1-dev libudev-dev llvm pkg-config zlib1g-dev curl
+sudo apt install -y build-essential clang cmake git libssl-dev libgtk-3-dev libsoup2.4-dev libwebkit2gtk-4.1-dev libudev-dev llvm pkg-config zlib1g-dev curl libgtk-4-dev libgraphene-1.0-dev
 
 # Build binary
 echo && echo "Cargo build"
-cargo build --release --manifest-path browser
+cargo build --release --manifest-path browser/Cargo.toml
