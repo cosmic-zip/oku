c79bd065b10062150448df474013d927da20d9c5
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-10-31 11:41:22 -0300

    Add lib js core

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo && echo "Install dependencies"
-sudo apt install -y build-essential clang cmake git libssl-dev libgtk-3-dev libsoup2.4-dev libwebkit2gtk-4.1-dev libudev-dev llvm pkg-config zlib1g-dev curl libgtk-4-dev libgraphene-1.0-dev
+sudo apt install -y build-essential clang cmake git libssl-dev libgtk-3-dev libsoup2.4-dev libwebkit2gtk-4.1-dev libudev-dev llvm pkg-config zlib1g-dev curl libgtk-4-dev libgraphene-1.0-dev libjavascriptcoregtk-6.0-dev
 
 # Build binary
 echo && echo "Cargo build"
